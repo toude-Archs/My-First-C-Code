af6e29ddfb5c7de5dd02efccab7b1172feef7872
Medhi Michel <100583551+toude-Archs@users.noreply.github.com>
2022-09-23 16:19:05 +0200

    Upload
    Is my first C code

---
+++ test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+Bienvenue sur Mon premier Fichier en C :)
+Hello World !
+
+*/
+
+int main(void)
+{
+    int nombre_entier = (int)123.44;
+    /*int ram = 0;// Ceci est un test. lol
+    float carteGraphique = 125.99;
+    signed char = 'A'
+
+    Ici Pour les printf
+    % : nombre entier (int)
+    % : nombre flottant (float)
+    % : caractère (char)
+    % : chaine de caractères (texte)
+*/
+    printf("")
+
+    return 0; 
+}
+
+/* 
+Actuellement je viens de créer une variable.
+*/
